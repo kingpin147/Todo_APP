4dfe5adf21c3ad00cdafaee7415b46d1cd7bab2e
Muhammad Nouman Attique <108257688+kingpin147@users.noreply.github.com>
2023-12-09 15:58:12 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,42 @@ import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
+    // title
     <div className="max-w-4xl mx-auto p-5">
       <h1 className="text-center text-[40px] underline">My Todo App</h1>
+      {/* input div */}
       <div className="flex justify-between gap-4">
-        <input className="w-[80%] p-2 ml-3  text-lg border-b focus: outline-none " placeholder="Write movie name" />
-        <button  className="bg-blue-700 w-[20%] text-white p-2 rounded-md  hover:bg-blue-200 hover:text-black"> Add movie </button>
+        <input
+          className="w-[60%] p-3 ml-3  text-lg border-b focus: outline-none "
+          placeholder="Write movie name"
+        />
+        <input
+          className="w-[20%] p-3 ml-3  text-lg border-b focus: outline-none "
+          placeholder="Write id"
+        />
+        {/* button */}
+        <button className="bg-blue-700 w-[20%] text-white p-2 rounded-md  hover:bg-blue-200 hover:text-black">
+          {" "}
+          Add movie{" "}
+        </button>
+      </div>
+      <h1 className="text-center text-[40px] underline mt-5 ">List</h1>
+      {/* grid */}
+      <div className="grid grid-cols-2 gap-5 mt-5">
+        {/* grid item */}
+        <div className="shadow p-4">
+          <div className="flex justify-between ">
+            <span className="shadow rounded-full h-8 w-8 text-center  my-auto">
+              1
+            </span>
+            <span className="shadow rounded-full h-8 w-8 text-center  my-auto">
+              X
+            </span>
+          </div>
+          <div className="mt-5 text-[30px] text-gray-700">Movies Name</div>
+          {/* data div */}
+        </div>
+        <div className="bg-blue-400">09</div>
       </div>
     </div>
   );
