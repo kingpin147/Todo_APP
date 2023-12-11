7828a246057b81b7c6b9a25c17490f901036dcf4
Muhammad Nouman Attique <108257688+kingpin147@users.noreply.github.com>
2023-12-11 15:47:48 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { useState } from "react";
 
 export default function Home() {
+  //define state
+  const [todos, setTodos] = useState([
+    { movie: "Django  Unchained", id: 1 },
+    { movie: "Catch me if you can",  id:2 },
+  ]);
+ const [inputVal, setInput]= useState("");
+ const [id , setId]:any = useState("");
+//functions
+const addItem  = ()=>{
+  setTodos([...todos,{movie:inputVal, id:id}])
+}
   return (
     // title
     <div className="max-w-4xl mx-auto p-5">
@@ -10,15 +23,21 @@ export default function Home() {
       {/* input div */}
       <div className="flex justify-between gap-4">
         <input
+        type ="text"
+        value={inputVal}
+        onChange={(e)=> setInput(e.target.value)}
           className="w-[60%] p-3 ml-3  text-lg border-b focus: outline-none "
           placeholder="Write movie name"
         />
         <input
+         type ="number"
+         value={id}
+         onChange={(e:any)=> setId(e.target.value)}
           className="w-[20%] p-3 ml-3  text-lg border-b focus: outline-none "
           placeholder="Write id"
         />
         {/* button */}
-        <button className="bg-blue-700 w-[20%] text-white p-2 rounded-md  hover:bg-blue-200 hover:text-black">
+        <button onClick={addItem} className="bg-blue-700 w-[20%] text-white p-2 rounded-md  hover:bg-blue-200 hover:text-black">
           {" "}
           Add movie{" "}
         </button>
@@ -27,19 +46,27 @@ export default function Home() {
       {/* grid */}
       <div className="grid grid-cols-2 gap-5 mt-5">
         {/* grid item */}
-        <div className="shadow p-4">
-          <div className="flex justify-between ">
-            <span className="shadow rounded-full h-8 w-8 text-center  my-auto">
-              1
-            </span>
-            <span className="shadow rounded-full h-8 w-8 text-center  my-auto">
-              X
-            </span>
-          </div>
-          <div className="mt-5 text-[30px] text-gray-700">Movies Name</div>
-          {/* data div */}
-        </div>
-        <div className="bg-blue-400">09</div>
+        {todos.map((item: any, index: any) => {
+          return (
+            <div className="shadow p-4" key={index}>
+              <div className="flex justify-between text-lg ">
+                <span className="cursor-pointer shadow rounded-full h-8 w-8 text-center my-auto">
+                  {index+1}
+                </span>
+                <span className="cursor-pointer shadow rounded-full h-8 w-8 text-center my-auto text-red-700">
+                  X
+                </span>
+              </div>
+              <div className="mt-5 text-[30px] text-gray-700">{item.movie}</div>
+              {/* data div */}
+              <div>
+                <h2 className="text-right cursor-pointer">Edit</h2>
+              </div>
+            </div>
+          );
+        })}
+
+        {/* <div className="bg-blue-400">09</div> */}
       </div>
     </div>
   );
